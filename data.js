2d3e00fae6cff05b0eefec4990d8f41be70e8103
Onyedikapaul <onyedikap429@gmail.com>
2026-05-06 22:06:44 +0100

    first commit

---
+++ data.js
@@ -0,0 +1,156 @@
+const products = [
+  {
+    name: "Barium Hydroxide",
+    category: "Intermediates",
+    image:
+      "https://thesolventchem.com/uploads/portfolio/barium-hydroxide-anhydrous-1000x1000_1768206554.jpg",
+  },
+  {
+    name: "Ammonium Thiocyanate",
+    category: "Intermediates",
+    image:
+      "https://thesolventchem.com/uploads/portfolio/Ammonium Thiocyanate_1768206436.jfif",
+  },
+  {
+    name: "Tall Oil Fatty Acid (TOFA)",
+    category: "Acid",
+    image:
+      "https://thesolventchem.com/uploads/portfolio/Tall Oil Fatty Acid (TOFA)_1768205722.jfif",
+  },
+  {
+    name: "Hexahydrophthalic Anhydride",
+    category: "Acid",
+    image:
+      "https://thesolventchem.com/uploads/portfolio/Hexahydrophthalic-anhydride-CAS-85-42-7-powder-2_1768205311.jpg",
+  },
+  {
+    name: "Erythorbic Acid",
+    category: "Acid",
+    image:
+      "https://thesolventchem.com/uploads/portfolio/sodiumpropionate_1768205134.jfif",
+  },
+  {
+    name: "Tetrabromophthalic Anhydride",
+    category: "Flame Retardants",
+    image:
+      "https://thesolventchem.com/uploads/portfolio/Screenshot (82)_1768204843.png",
+  },
+  {
+    name: "Magnesium Sulfate",
+    category: "Flame Retardants",
+    image:
+      "https://thesolventchem.com/uploads/portfolio/magnesium-sulphate-heptahydrate_1768204254.jpg",
+  },
+  {
+    name: "Calcium Chloride",
+    category: "Flame Retardants",
+    image:
+      "https://thesolventchem.com/uploads/portfolio/calcium-chloride_1768201808.jfif",
+  },
+  {
+    name: "Methyl Ethyl Ketone (MEK)",
+    category: "C.A.S.E",
+    image:
+      "https://thesolventchem.com/uploads/portfolio/methyl-ethyl-ketone-1000x1000_1768201346.jpg",
+  },
+  {
+    name: "Glycerine",
+    category: "C.A.S.E",
+    image:
+      "https://thesolventchem.com/uploads/portfolio/Iran2africaindustrial-grade-glycerin-Picture_1768200160.jpg",
+  },
+  {
+    name: "Ethylene Glycol (MEG)",
+    category: "C.A.S.E",
+    image:
+      "https://thesolventchem.com/uploads/portfolio/mono-ethylene-glycol-meg--032_1768200007.jpg",
+  },
+  {
+    name: "Cerium Oxide",
+    category: "Flame Retardants",
+    image:
+      "https://thesolventchem.com/uploads/portfolio/cerium-oxide_1768198075.jfif",
+  },
+  {
+    name: "Resorcinol",
+    category: "Polymer Additives",
+    image:
+      "https://thesolventchem.com/uploads/portfolio/images (9)_1768031997.png",
+  },
+  {
+    name: "Potassium Phosphate",
+    category: "Fertilizer/Crop Nutrition",
+    image:
+      "https://thesolventchem.com/uploads/portfolio/images (33)_1768031848.jpeg",
+  },
+  {
+    name: "Benzoic Acid",
+    category: "Intermediates",
+    image:
+      "https://thesolventchem.com/uploads/portfolio/benzoic-acid_1768031692.jpg",
+  },
+  {
+    name: "Methanol",
+    category: "Water Treatment",
+    image:
+      "https://thesolventchem.com/uploads/portfolio/images (32)_1768031467.jpeg",
+  },
+  {
+    name: "Ascorbic Acid",
+    category: "Acid",
+    image:
+      "https://thesolventchem.com/uploads/portfolio/buy-ascorbic-acid-vitamin-c-powder-in-nigeria_1768031330.jpg",
+  },
+  {
+    name: "Ammonium Polyphosphate",
+    category: "Flame Retardants",
+    image:
+      "https://thesolventchem.com/uploads/portfolio/ammonium-polyphosphate-cas-68333-79-9_1768031146.jpg",
+  },
+  {
+    name: "Diethylene Glycol (DEG)",
+    category: "C.A.S.E",
+    image:
+      "https://thesolventchem.com/uploads/portfolio/d-e-g-diethylene-glycol--500x500_1768031010.jpg",
+  },
+  {
+    name: "Benzyl Alcohol",
+    category: "C.A.S.E",
+    image:
+      "https://thesolventchem.com/uploads/portfolio/benzyl-alcohol_1768029745.jpg",
+  },
+  {
+    name: "Acetone",
+    category: "C.A.S.E",
+    image:
+      "https://thesolventchem.com/uploads/portfolio/images (30)_1767946334.jpeg",
+  },
+  {
+    name: "Alumina Trihydrate (ATH)",
+    category: "Flame Retardants",
+    image:
+      "https://thesolventchem.com/uploads/portfolio/images (8)_1767946454.png",
+  },
+  {
+    name: "Silanes",
+    category: "Silanes",
+    image: "https://thesolventchem.com/uploads/portfolio/SIH4_1767946901.jpg",
+  },
+  {
+    name: "2-Ethylhexanoic Acid",
+    category: "Acid",
+    image: "https://thesolventchem.com/uploads/portfolio/E0120_1767947077.jpg",
+  },
+  {
+    name: "Ammonium Bromide",
+    category: "Water Treatment",
+    image:
+      "https://thesolventchem.com/uploads/portfolio/ammonium-bromide-500x500_1767947220.jpg",
+  },
+  {
+    name: "Ammonium Chloride",
+    category: "Fertilizer/Crop Nutrition",
+    image:
+      "https://thesolventchem.com/uploads/portfolio/images (31)_1767947399.jpeg",
+  },
+];
